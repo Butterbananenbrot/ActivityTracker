6045c53065370217d83493ec6ca33e3c25365e94
Lezin901 <l.axmann901@gmail.com>
2025-07-30 14:05:06 +0200

    Created SleepInterval model

--- sleeptime/models.py
+++ sleeptime/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+
+class SleepInterval(models.Model):
+    # Klassenattribut
+    SLEEPING_PLACE_CHOICES = [
+        ("B", "Bed"),
+        ("S", "Sofa"),
+        ("F", "Floor"), # Damit eine dritte Wahl vorhanden ist
+    ]
+    # Umwandeln in ein Dictionary, damit in __str__ nutzbar
+    SLEEPING_PLACE_CHOICES_MAP = dict(SLEEPING_PLACE_CHOICES) # Klassenattribut
+
+
+    start_time = models.DateTimeField()
+    end_time = models.DateTimeField()
+    sleeping_place = models.CharField(max_length=1, choices=SLEEPING_PLACE_CHOICES)
+    recreation = models.IntegerChoices("Recreation", "1 2 3 4 5 6 7 8 9 10")
+    tiredness_before_sleeping = models.IntegerChoices("Tiredness", "1 2 3 4 5 6 7 8 9 10")
+
+    def __str__(self):
+        # Da wir die Methode auf einem Objekt aufrufen,
+        # müssen wir SLEEPING_PLACE_CHOICES_MAP als Klassenattribut abrufen
+        return SleepInterval.SLEEPING_PLACE_CHOICES_MAP.get(self.sleeping_place)
