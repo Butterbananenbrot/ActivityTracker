0aa02e5140d98ea1786823bbaf7e4464c442b659
Butterbanenbrot <jh.hagedorn@gmail.com>
2025-08-03 15:38:40 +0200

    Automatisierte Tests für breaktime eingebaut

--- breaktime/models.py
+++ breaktime/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+import datetime
+from auxiliary.choices import ONE_TO_TEN_SCALE
 
 class Break(models.Model):
     ACTIVITY_CHOICES = [
@@ -15,9 +17,23 @@ class Break(models.Model):
     ]
     start_time = models.DateTimeField()
     end_time = models.DateTimeField()
-    activity = models.CharField(max_length=2, choices=ACTIVITY_CHOICES, default=1)
-    #activity = models.CharField("Activity", choices=["Walk", "Sleep", "Eat", "Smalltalk"], default="Smalltalk")
-    place = models.CharField(max_length=1, choices=PLACE_CHOICES, default=1)
-    recreation = models.IntegerChoices("RECREATION", "1 2 3 4 5 6 7 8 9 10")
+    activity = models.CharField(max_length=2, choices=ACTIVITY_CHOICES, default="WA")
+    # activity = models.CharField("Activity", choices=["Walk", "Sleep", "Eat", "Smalltalk"], default="Smalltalk")
+    place = models.CharField(max_length=1, choices=PLACE_CHOICES, default="P")
+    recreation = models.IntegerField(choices=ONE_TO_TEN_SCALE, default=5) # import from aux module
+
     def __str__(self):
         return self.activity
+
+    def break_took_max_eight_hours(self ):
+        # Validierungsmethode: TRUE bei korrektem Input
+        check = self.end_time - self.start_time < datetime.timedelta(hours=8)
+        return check
+
+    def end_time_after_start_time(self):
+        # Validierungsmethode: TRUE bei korrektem Input
+        return self.end_time > self.start_time
+
+    def rec_value_between_one_and_ten(self):
+        # Validierungsmethode für import der Scale aus choices.py: TRUE bei korrektem Input
+        return 1 <= self.recreation <= 10
--- breaktime/tests.py
+++ breaktime/tests.py
@@ -1,3 +1,35 @@
-from django.test import TestCase
+import datetime
 
-# Create your tests here.
+from django.test import TestCase
+from django.utils import timezone
+
+from .models import Break
+
+
+class BreakModelTests(TestCase):
+    def test_break_took_max_eight_hours(self):
+        """
+        Prüfen der Start- und Endzeiten: immer maximal 8 Stunden Pause?
+        break_took_max_eight_hours() returns False for questions whose length is more than eight hours.
+        """
+        overlong_break = Break(start_time=timezone.now() - datetime.timedelta(hours=10),
+                               end_time=timezone.now() - datetime.timedelta(hours=1))
+        self.assertIs(overlong_break.break_took_max_eight_hours(), False)
+
+    def test_end_time_after_start_time(self):
+        """
+        Prüft, ob eine Pause die vor 1 Stunde angefangen und vor 10 Stunden geendet hat
+        wie vorgesehen als Falsch erkannt wird
+        """
+        impossible_times_break = Break(start_time=timezone.now() - datetime.timedelta(hours=1),
+                                       end_time=timezone.now() - datetime.timedelta(hours=10))
+        self.assertIs(impossible_times_break.end_time_after_start_time(), False)
+
+    def test_rec_value_between_one_and_ten(self):
+        """
+        Prüft, ob die Recreation wie vorgesehen zwischen eins und zehn ist
+        :return: TRUE wenn Test bestanden
+        """
+        outside_bounds_recreation = Break(start_time=timezone.now()- datetime.timedelta(hours=1),
+                                          end_time=timezone.now(), recreation=11)
+        self.assertIs(outside_bounds_recreation.rec_value_between_one_and_ten(), False)
