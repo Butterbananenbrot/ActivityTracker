273f87cf2cdecde123afb38c4ae2aa4f08bcbb75
Lezin901 <l.axmann901@gmail.com>
2025-08-05 12:33:45 +0200

    Grundsätzliche Tests für sleeptime erstellt

--- sleeptime/models.py
+++ sleeptime/models.py
@@ -1,3 +1,5 @@
+import datetime
+
 from django.db import models
 from auxiliary.choices import ONE_TO_TEN_SCALE
 
@@ -14,7 +16,7 @@ class SleepInterval(models.Model):
     SLEEPING_PLACE_CHOICES_MAP = dict(SLEEPING_PLACE_CHOICES) # Klassenattribut
     start_time = models.DateTimeField()
     end_time = models.DateTimeField()
-    sleeping_place = models.CharField(max_length=1, choices=SLEEPING_PLACE_CHOICES)
+    sleeping_place = models.CharField(max_length=1, choices=SLEEPING_PLACE_CHOICES, default="B")
     recreation = models.IntegerField(choices=ONE_TO_TEN_SCALE, default=1)
     tiredness_before_sleeping = models.IntegerField(choices=ONE_TO_TEN_SCALE, default=1)
 
@@ -22,3 +24,12 @@ class SleepInterval(models.Model):
         # Da wir die Methode auf einem Objekt aufrufen,
         # müssen wir SLEEPING_PLACE_CHOICES_MAP als Klassenattribut abrufen
         return SleepInterval.SLEEPING_PLACE_CHOICES_MAP.get(self.sleeping_place)
+
+    def sleep_interval_is_shorter_than_20_hours(self):
+        # Validierungsmethode: TRUE bei korrektem Input
+        check = self.end_time - self.start_time < datetime.timedelta(hours=20)
+        return check
+
+    def end_time_after_start_time(self):
+        # Validierungsmethode: TRUE bei korrektem Input
+        return self.end_time > self.start_time
--- sleeptime/tests.py
+++ sleeptime/tests.py
@@ -1,3 +1,27 @@
+import datetime
+
 from django.test import TestCase
 
 # Create your tests here.
+from django.test import TestCase
+from django.utils import timezone
+
+from .models import SleepInterval
+
+class DrinkModelTest(TestCase):
+    def test_sleep_interval_is_shorter_than_20_hours(self):
+        """
+        Prüfen der Start- und Endzeiten: immer maximal 20 Stunden Schlaf nur?
+        sleep_interval_is_shorter_than_20_hours() gibt False zurück für Schlafintervalle, die länger als 20 Stunden sind.
+        """
+        overlong_sleep_interval = SleepInterval(start_time=timezone.now() - datetime.timedelta(hours=100),
+                               end_time=timezone.now() - datetime.timedelta(hours=1))
+        self.assertIs(overlong_sleep_interval.sleep_interval_is_shorter_than_20_hours(), False)
+    def test_end_time_after_start_time(self):
+        """
+        Prüft, ob ein Schlafintervall, welcher vor einer Stunde angefangen hat und vor zehn Stunden geendet hat
+        wie vorgesehen als Falsch erkannt wird
+        """
+        impossible_sleep_interval = SleepInterval(start_time=timezone.now() - datetime.timedelta(hours=1),
+                                       end_time=timezone.now() - datetime.timedelta(hours=10))
+        self.assertIs(impossible_sleep_interval.end_time_after_start_time(), False)
