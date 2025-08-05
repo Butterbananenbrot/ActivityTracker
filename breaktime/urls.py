ff4fa923eb4a09ffe7bb8fe70f3b42c541ebaf3b
Lezin901 <l.axmann901@gmail.com>
2025-08-05 15:42:38 +0200

    ListView eingebaut, Datenbank Einträge ergänzt für Tabellenansicht. Templates eingebaut.

--- breaktime/urls.py
+++ breaktime/urls.py
@@ -7,5 +7,6 @@ from . import views
 
 app_name = "breaktime"
 urlpatterns = [
-    path("", views.welcome_page, name="index")
+    path("", views.welcome_page, name="index"),
+    path("break_list/", views.BreakListView.as_view(), name="break_list")
 ]
--- breaktime/views.py
+++ breaktime/views.py
@@ -1,8 +1,22 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.views import generic
+from .models import Break
 
 # Create your views here.
 def welcome_page(request):
     # return HttpResponse("Welcome to breaktime app." )
     return render(request, 'breaktime/index.html')
+
+
+class BreakListView(generic.ListView):
+    model = Break
+    context_object_name = "break_list_context"
+
+    template_name = "breaktime/break_list.html"
+
+    def get_queryset(self):
+        return Break.objects.all()[0:4 + 1]
+
+
 
