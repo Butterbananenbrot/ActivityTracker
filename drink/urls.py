a128db94f9e217a18a815c0d95d2356b60a3a902
Butterbanenbrot <jh.hagedorn@gmail.com>
2025-08-07 13:13:33 +0200

    fixed urls and html templates to work with new generic view

--- drink/urls.py
+++ drink/urls.py
@@ -9,5 +9,5 @@ app_name = "drink"
 urlpatterns = [
     path("", views.welcome_page, name="index"),
     path("drink_list/", views.DrinkListView.as_view(), name="drink_list"),
-    path("drink_table_view/", views.drink_table_view, name="break_table_view")
+    path("drink_table_view/", views.drink_table_view, name="drink_table_view")
 ]
