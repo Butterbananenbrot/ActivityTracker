a42ca97ef25b8a6abc14ec56e3b16272c0b502fd
Butterbanenbrot <jh.hagedorn@gmail.com>
2025-08-07 02:40:30 +0200

    added drink table using ListView

--- drink/urls.py
+++ drink/urls.py
@@ -7,5 +7,6 @@ from . import views
 
 app_name = "drink"
 urlpatterns = [
-    path("", views.welcome_page, name="index")
+    path("", views.welcome_page, name="index"),
+    path("drink_list/", views.DrinkListView.as_view(), name="drink_list")
 ]
--- drink/views.py
+++ drink/views.py
@@ -1,7 +1,20 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.views import generic
+
+from drink.models import Drink
+
 
 # Create your views here.
 def welcome_page(request):
     # return HttpResponse("Welcome to breaktime app." )
     return render(request, 'drink/index.html')
+
+class DrinkListView(generic.ListView):
+    model = Drink
+    context_object_name = "drink_list_context"
+
+    template_name = "drink/drink_list.html"
+
+    def get_queryset(self):
+        return Drink.objects.all()[0:4 + 1]
