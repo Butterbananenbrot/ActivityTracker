68957847ebaf64eeb6a5146eb655f4f3992e16af
Lezin901 <l.axmann901@gmail.com>
2025-08-30 11:29:08 +0200

    break-App: Tabelle in View eingebaut

--- auxiliary/context_generator.py
+++ auxiliary/context_generator.py
@@ -1,5 +1,5 @@
 from drink.models import Drink
-
+from breaktime.models import Break
 
 def create_drink_data_context():
     mapping = {"W": "Water", "C": "Coffee", "B": "Beer"}
@@ -12,3 +12,17 @@ def create_drink_data_context():
         for d in Drink.objects.all()
     ]
     return drinks
+
+
+def create_break_data_context():
+    breaks = [
+        {
+            "start_time": b.start_time,
+            "end_time": b.end_time,
+            "activity": b.activity,
+            "place": b.place,
+            "recreation": b.recreation
+        }
+        for b in Break.objects.all()
+    ]
+    return breaks
--- breaktime/views.py
+++ breaktime/views.py
@@ -2,14 +2,19 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from django.views import generic
 
+from auxiliary.context_generator import create_break_data_context
 from auxiliary.view_generator import generate_table_view
 from .models import Break
 
-# Create your views here.
-def welcome_page(request):
-    # return HttpResponse("Welcome to breaktime app." )
-    return render(request, 'breaktime/index.html')
+# # Create your views here.
+# def welcome_page(request):
+#     # return HttpResponse("Welcome to breaktime app." )
+#     return render(request, 'breaktime/index.html')
 
+
+def welcome_page(request):
+    breaks = create_break_data_context()
+    return render(request, 'breaktime/index.html', {"break_data_context": breaks})
 
 class BreakListView(generic.ListView):
     ''' Veralteter View, stattdessen wird mittlerweile break_table_view genutzt. '''
